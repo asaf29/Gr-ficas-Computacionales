fdbe1996771122b783eb3c8c63b1aa6d1e54dde9
Kevin <kevin.asaf@gmail.com>
2017-10-23 15:46:03 -0500

    Examen 2do Parcial

--- Graficas Computacionales/Graficas Computacionales/Main.cpp
+++ Graficas Computacionales/Graficas Computacionales/Main.cpp
@@ -1,6 +1,7 @@
 /*********
 Kevin Asaf Alvarez Villarruel A01376017	
 Isaac Hinojosa Padilla A01375843
+Daniel Schacht Luna A01169574
 
 ******/
 
@@ -18,160 +19,38 @@ Mesh _mesh;
 ShaderProgram _shaderProgram;
 Transform _transform;
 Transform _transform1;
-Transform _transform2;
-Transform _transform3;
-Transform _transform4;
-Transform _transform5;
+
 Camera _camera;
+
+
+float i = 0.0f;
+float ii = 0.0f;
+bool limite = true;
 
 void Initialize()
 {
 	std::vector<glm::vec3> positions;
 
-	//Frente 
-	positions.push_back(glm::vec3(-3.0f, -3.0f, 3.0f)); //0
-	positions.push_back(glm::vec3(3.0f, -3.0f, 3.0f)); //1
-	positions.push_back(glm::vec3(3.0f, 3.0f, 3.0f)); //2
-	positions.push_back(glm::vec3(-3.0, 3.0f, 3.0f)); //3
-	//Derecha
-	positions.push_back(glm::vec3(3.0f, -3.0f, 3.0f)); //4
-	positions.push_back(glm::vec3(3.0f, -3.0f, -3.0f)); //5
-	positions.push_back(glm::vec3(3.0f, 3.0f, -3.0f)); //6
-	positions.push_back(glm::vec3(3.0f, 3.0f, 3.0f)); //7
-	//Izquierda 
-	positions.push_back(glm::vec3(-3.0f, -3.0f, -3.0f)); //8
-	positions.push_back(glm::vec3(-3.0f, -3.0f, 3.0f)); //9
-	positions.push_back(glm::vec3(-3.0f, 3.0f, 3.0f)); //10
-	positions.push_back(glm::vec3(-3.0f, 3.0f, -3.0f)); //11
-	//Atras 
-	positions.push_back(glm::vec3(3.0f, -3.0f, -3.0f)); //12
-	positions.push_back(glm::vec3(-3.0f, -3.0f, -3.0f)); //13
-	positions.push_back(glm::vec3(-3.0f, 3.0f, -3.0f)); //14
-	positions.push_back(glm::vec3(3.0f, 3.0f, -3.0f)); //15
-	//Arriba 
-	positions.push_back(glm::vec3(-3.0f, 3.0f, 3.0f)); //16
-	positions.push_back(glm::vec3(3.0f, 3.0f, 3.0f)); //17
-	positions.push_back(glm::vec3(3.0f, 3.0f, -3.0f)); //18
-	positions.push_back(glm::vec3(-3.0f, 3.0f, -3.0f)); //19
-	//Abajo
-	positions.push_back(glm::vec3(-3.0f, -3.0f, -3.0f)); //20
-	positions.push_back(glm::vec3(3.0f, -3.0f, -3.0f)); //21
-	positions.push_back(glm::vec3(3.0f, -3.0f, 3.0f)); //22
-	positions.push_back(glm::vec3(-3.0f, -3.0f, 3.0f)); //23
-
-
+	//Triangulo
+	positions.push_back(glm::vec3(0.0f, 1.0f, 0.0f)); //0
+	positions.push_back(glm::vec3(-1.0f, -1.0f, 1.0f)); //1
+	positions.push_back(glm::vec3(1.0f, -1.0f, 1.0f)); //2
+	positions.push_back(glm::vec3(1.0, -1.0f, -1.0f)); //3
+	positions.push_back(glm::vec3(-1.0f, -1.0f, -1.0f));//4
 
 	// Arreglo de colores en el cpu
 	std::vector<glm::vec3> colors;
-	//Frente 
-	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
-	//Derecha
+
 	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
-	//Izquierda 
 	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
-	//Atras
-	colors.push_back(glm::vec3(1.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(1.0f, 1.0f, 0.0f));
-	//Arriba
-	colors.push_back(glm::vec3(0.0f, 1.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 1.0f));
-	//Abajo
-	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(0.0f, 0.0f, 0.0f));
 
-
-
-	//0, 1, 2, 0, 2, 3,   //front 
-	//4, 5, 6, 4, 6, 7,   //right 
-	//8, 9, 10, 8, 10, 11,  //back 
-	//12, 13, 14, 12, 14, 15,  //left 
-	//16, 17, 18, 16, 18, 19,  //upper 
-	//20, 21, 22, 20, 22, 23
-
-	std::vector<unsigned int> indices = { 0,1,2,0,2,3,
-		4,5,6,4,6,7,
-		8,9,10,8,10,11,
-		12,13,14,12,14,15,
-		16,17,18,16,18,19,
-		20,21,22,20,22,23 };
-	/*****Frente 
-	indices.push_back(0);
-	indices.push_back(1);
-	indices.push_back(2);
-	indices.push_back(0); 
-	indices.push_back(2); 
-	indices.push_back(3);
-	//CDerecha
-	indices.push_back(4); 
-	indices.push_back(5); 
-	indices.push_back(6);
-	indices.push_back(4); 
-	indices.push_back(6); 
-	indices.push_back(7);
-	//Izquierda
-	indices.push_back(8); 
-	indices.push_back(9); 
-	indices.push_back(10);
-	indices.push_back(8);
-	indices.push_back(10); 
-	indices.push_back(11);
-	// Atras
-	indices.push_back(12);
-	indices.push_back(13); 
-	indices.push_back(14);
-	indices.push_back(12);
-	indices.push_back(14);
-	indices.push_back(15);
-	// Arriba
-	indices.push_back(16);
-	indices.push_back(17); 
-	indices.push_back(18);
-	indices.push_back(16); 
-	indices.push_back(18);
-	indices.push_back(19);
-	// Abajo
-	indices.push_back(20); 
-	indices.push_back(21);
-	indices.push_back(22);
-	indices.push_back(20); 
-	indices.push_back(22);
-	indices.push_back(23);
-
-	****/
-	//triangulo piso
-	_transform1.Translate(0.0f, -5.0f, 0.0f, false);
-	_transform1.SetScale(20.0f, 0.0f, 20.0f);
-
-	//base torre
-	_transform2.Translate(17.0f, 0.0f, 17.0f, false);
-	_transform2.SetScale(1.0f, 1.0f, 1.0f);
-	
-	//tapa torre
-	_transform3.Translate(17.0f, 6.0f, 17.0f, false);
-
-	//CUBO R X
-	_transform4.Translate(0.0f, 0.0f, 30.0f, false);
-
-	//CUBO R Z
-	_transform5.Translate(-30.0f, 0.0f, 10.0f, false);
+	std::vector<unsigned int> indices = { 0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 1, 1, 4, 3, 1, 3, 2};
 	
 
-	_mesh.CreateMesh(24);
+	_mesh.CreateMesh(5);
 	_mesh.SetIndices(indices, GL_STATIC_DRAW);
 
 	_mesh.SetPositionAttribute(positions, GL_STATIC_DRAW, 0);
@@ -183,50 +62,73 @@ void Initialize()
 	_shaderProgram.SetAttribute(0, "VertexPosition");
 	_shaderProgram.SetAttribute(1, "VertexColor");
 	_shaderProgram.LinkProgram();
+	
 
-	//_transform.SetRotation(0.0f, 0.0f, 45.0f);
+	//Piramide grande
+	_transform.SetScale(3.0f, 3.0f, 3.0f);
 }
 
 void GameLoop()
 {
+
+	
+
 	// Limpiamos el buffer de color y el buffer de profunidad.
 	// Siempre hacerlo al inicio del frame
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	_camera.SetPosition(0.0f, 0.0f, 15.0f);
-	_camera.Rotate(0.0f, 1.0f, 0.0f, false);
+	_camera.SetPosition(0.0f, 0.0f, 25.0f);
 
-	//_camera.MoveForward(0.0001f);
+
+	//_camera.MoveForward(0.0001f)
+
+	
+
+
 	_transform.Rotate(0.3f, 0.3f, 0.3f, true);
 
+	float r = 5.0f;
+	float tetha = glm::radians(i),	x = r*(glm::cos(tetha)), y = r*(glm::sin(tetha)), z = 0.0f;
+
+	_transform.SetPosition(x, y, z);
+	i = i + 1.0f;
+
 	_shaderProgram.Activate(); //mandar informacion, activar shader
+
 	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection() * _transform.GetModelMatrix()); //getmodelmatrix, donde esta que escala tiene que rotacion tiene
 	_mesh.Draw(GL_TRIANGLES); //instruccion de dibujado, para hacer una nueva figura se debe usar otro draw
 
-	//Cuadrado piso
+	//segundo triangulo
 	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection() * _transform1.GetModelMatrix());
 	_mesh.Draw(GL_TRIANGLES);
 
-	//base torre
-	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection() * _transform2.GetModelMatrix());
-	_mesh.Draw(GL_TRIANGLES);
-	_transform2.Rotate(0.0f, 1.0f, 0.0f, true);
+	_transform1.Rotate(-0.3f, -0.3f, -0.3f, true);
 
-	//tapa torre
-	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection() * _transform3.GetModelMatrix());
-	_mesh.Draw(GL_TRIANGLES);
-	_transform3.Rotate(0.0f, -1.0f, 0.0f, true);
+	float size = 0.5f + ii;
+	
 
-	//CUBO R x
-	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection() * _transform4.GetModelMatrix());
-	_mesh.Draw(GL_TRIANGLES);
-	_transform4.Rotate(1.0f, 0.0f, 0.0f, true);
+	if (size <= 1.0f & limite == true)
+	{
+		_transform1.SetScale(size, size, size);
+		ii = ii + 0.005f;
+	}
+	else if (size >= 1.0f & limite == true)
+	{
+		limite = false;
+	}
+	else if (size >= 0.25f & limite == false)
+	{
+		_transform1.SetScale(size, size, size);
+		ii = ii - 0.005f;
+	}
+	else if (size <= 0.25f & limite == false)
+	{
+		limite = true;
+	}
 
-	//CUBO R Z
-	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection() * _transform5.GetModelMatrix());
-	_mesh.Draw(GL_TRIANGLES);
-	_transform5.Rotate(0.0f, 0.0f, 1.0f, true);
 
+
+	
 
 
 	_shaderProgram.Deactivate();
